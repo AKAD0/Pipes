e0ad80f6300ac65c0d326eb6688b96c6fbc5e27b
AKAD0 <onixon2010@gmail.com>
2025-03-05 05:09:27 +0300

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
    
      Initial commit
    
      Changes to be committed:
     	new file:   .gitignore
     	new file:   1.ipynb
     	new file:   README_task.md
     	new file:   data.csv
     	new file:   readme.md
     	new file:   setup.bat
     	new file:   target_train.csv

---
+++ setup.bat
@@ -0,0 +1,15 @@
+@echo off
+REM set current directory
+set DIR=%~dp0
+cd /d %DIR%
+
+REM install libs
+"python.exe" -m venv venv
+call .\venv\Scripts\activate.bat
+pip install numpy pandas matplotlib scikit-learn
+
+REM add .gitignore file
+echo. > .gitignore
+echo venv/ >> .gitignore
+
+pause

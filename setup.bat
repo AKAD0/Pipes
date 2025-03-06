ddbcb9b3c70920d278a4e0f0ffcf41ce400f5aaa
AKAD0 <onixon2010@gmail.com>
2025-03-06 14:11:13 +0300

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Changes to be committed:
     	modified:   1.ipynb
     	modified:   setup.bat

--- setup.bat
+++ setup.bat
@@ -6,7 +6,7 @@ cd /d %DIR%
 REM install libs
 "python.exe" -m venv venv
 call .\venv\Scripts\activate.bat
-pip install numpy pandas matplotlib scikit-learn
+pip install numpy pandas matplotlib scikit-learn scipy
 
 REM add .gitignore file
 echo. > .gitignore
